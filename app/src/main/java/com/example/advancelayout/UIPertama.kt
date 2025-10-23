b5018231feee21a527ccdb22534fae01b8e1f575
Naufal0067 <naufalopang1928@gmail.com>
2025-10-23 12:26:21 +0700

    menambahkan box

--- app/src/main/java/com/example/advancelayout/UIPertama.kt
+++ app/src/main/java/com/example/advancelayout/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.advancelayout
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -64,6 +65,12 @@ fun ActivityPertama(modifier: Modifier) {
                             stringResource(R.string.alamat),
                             fontSize = 20.sp,
                         )
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        Text()
                     }
 
                 }
