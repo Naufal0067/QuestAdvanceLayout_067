d9217c33b25dc0ce21f2a1781f7659640fb53f57
Naufal0067 <naufalopang1928@gmail.com>
2025-10-23 12:54:40 +0700

    memperbaiki error

--- app/src/main/java/com/example/advancelayout/UIPertama.kt
+++ app/src/main/java/com/example/advancelayout/UIPertama.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.res.painterResource
