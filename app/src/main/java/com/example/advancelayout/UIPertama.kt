89ac33fd308597fa387475b5f6f226198ddd47db
Naufal0067 <naufalopang1928@gmail.com>
2025-10-23 11:54:44 +0700

    menambahkan column horizontal untuk text dengan id prodi dan univ

--- app/src/main/java/com/example/advancelayout/UIPertama.kt
+++ app/src/main/java/com/example/advancelayout/UIPertama.kt
@@ -1,3 +1,31 @@
 package com.example.advancelayout
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+
+@Composable
+fun ActivityPertama(modifier: Modifier) {
+    Column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Text(stringResource( R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
+        Text(stringResource(R.string.univ),
+            fontSize = 22.sp)
+        
+    }
+}
