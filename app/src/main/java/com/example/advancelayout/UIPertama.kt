2ce305f00c636433c17fac0bb0f5b74d4c5614eb
Naufal0067 <naufalopang1928@gmail.com>
2025-10-23 12:15:19 +0700

    menambahkan gambar dan code untuk posisi gambar tersebut

--- app/src/main/java/com/example/advancelayout/UIPertama.kt
+++ app/src/main/java/com/example/advancelayout/UIPertama.kt
@@ -1,6 +1,8 @@
 package com.example.advancelayout
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -9,10 +11,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,11 +36,21 @@ fun ActivityPertama(modifier: Modifier) {
             fontSize = 22.sp)
         Spacer(modifier = Modifier.height(25.dp))
         Card(modifier = Modifier
-            .fillMaxSize(1f )
+            .fillMaxWidth(1f )
             .padding(12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.Black
             )) {
+            Row() {
+                Column {
+                    val gambar = painterResource(R.drawable.logo_umy)
+                    Image(
+                        painter = gambar,
+                        contentDescription = null,
+                        modifier = modifier.size(100.dp).padding(5.dp)
+                    )
+                }
+            }
 
         }
     }
