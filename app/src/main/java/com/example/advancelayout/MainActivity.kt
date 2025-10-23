567d0ee0b3fa6d62f681b5d89129f252e1d83330
Naufal0067 <naufalopang1928@gmail.com>
2025-10-23 13:02:12 +0700

    mengganti greetings menjadi activity pertama di main acitivity

--- app/src/main/java/com/example/advancelayout/MainActivity.kt
+++ app/src/main/java/com/example/advancelayout/MainActivity.kt
@@ -20,28 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             AdvanceLayoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivityPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AdvanceLayoutTheme {
-        Greeting("Android")
     }
 }
